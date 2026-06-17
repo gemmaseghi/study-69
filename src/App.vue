ef8466a19f2554d61e99e4af876bfc9eabe4d6bb
Gemma <gemma.seghi@uni-tuebingen.de>
2026-06-17 23:23:18 +0200

    added a back button and changed the flowers

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,7 @@
 <template>
   <Experiment title="Perspective-taking experiment">
-    <InstructionScreen :title="'Welcome'">
-      Welcome to the experiment! You will first read a set of instructions. The instructions are divided into four parts. Please move to the next screen to read the first part.
-    </InstructionScreen>
 
-    <InstructionScreen :title="'Instructions'">
-      In this experiment, you will play a game with another participant. You will play the role of the listener, while the other participant will play the role of the speaker. 
-      <br><br>
-      On each trial, you will see a grid containing four objects and a description above it. The description is produced by the speaker to indicate one of the objects in the grid. You will only be presented the speaker's description in written form, you won't hear it spoken aloud.
-      
-    </InstructionScreen>
-
-    <InstructionScreen :title="'Instructions'">
-      The speaker sees a slightly different version of the grid. Their grid contains only three objects because one cell is hidden by an occluder. The location of the hidden cell remains the same throughout the experiment.
-
-      <br><br>
-
-      Apart from the hidden object, both grids contain the same objects. However, the position of the visible objects is shuffled across the two grids, so the speaker cannot use location words (e.g., "top left" or "bottom right") to refer to an object.
-    
-    </InstructionScreen>
-
-    <InstructionScreen :title="'Instructions'">
-      On the speaker's screen, one of the visible objects is marked with an asterisk (*). The speaker's task is to describe that object. Your task is to use the description, which will appear above the grid, to determine which object the speaker intended to refer to and select it.
-
-      <br><br>
-      
-      In some cases, you might not have enough information to identify a single object. When this happens, please select only the object that you think is the most likely candidate based on the information you have.
-      
-    </InstructionScreen>
-    <InstructionScreen :title="'Instructions'">
-      Occasionally, you will be asked whether you think you have enough information to determine which cell is hidden from the speaker. If you believe you have enough information, you will be asked to select the hidden cell in an empty grid. 
-
-      <br><br>
-
-      If you correctly identify the hidden cell, that cell will be marked in grey for the remainder of the experiment, so you can use that information to guide your selections in the remaining trials. Continue selecting the object you think the speaker is referring to until the end of the experiment.
-      <br><br>
-      At the end of the experiment, you will be asked to complete a short questionnaire.
-    </InstructionScreen>
-
-    <InstructionScreen :title="'Example'">
-      <img
-        src="instructions/example_view.png"
-        style="width: 800px; max-width: 100%;"
-      />
-
-      <p>
-        Example of the speaker's view (left) and your view (right).
-        The grey cell indicates the object that is hidden from the speaker.
-        The asterisk (*) marks the object that the speaker has to describe.
-      </p>
-
-      <p>
-        During the experiment, you will only see the right view. The speaker's view is shown here only to illustrate how the game works.
-        Your task is to click on the object that you think the speaker is referring to and to infer which cell is hidden from them.
-      </p>
-    </InstructionScreen>
+    <InstructionsWithBack />
 
     <PracticeBlock
       :practiceTrials="practiceTrials"
@@ -87,6 +34,7 @@
 <script>
 import _ from "lodash";
 import trials from "./trials";
+import InstructionsWithBack from "./InstructionsWithBack.vue";
 import practiceTrials from "./practiceTrials";
 import GridTrial from "./GridTrial.vue";
 import BlindSpotProbe from "./BlindSpotProbe.vue";
@@ -97,6 +45,7 @@ import FinalBlindSpotProbe from "./FinalBlindSpotProbe.vue";
 export default {
   name: "App",
   components: {
+    InstructionsWithBack,
     GridTrial,
     PracticeBlock,
     BlindSpotProbe,
